00cb6d57bbb9e9b662c7271fff1d260786328023
Lucaschen1113 <N76094427@gs.ncku.edu.tw>
2022-01-25 23:13:49 +0800

    Successfully download captions and save video list to file

--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -6,14 +6,18 @@ from yt_concate.pipeline.settings import API_KEY
 
 
 class GetVideoList(Step):
-    def process(self, data, inputs):
+    def process(self, data, inputs, utils):
         channel_id = inputs['channel_id']
+
+        if utils.video_list_file_exists(channel_id):
+            print('video_list_file_exists(UCKSVUHI9rbbkXhvAXK-2uxA)')
+            return self.read_file(utils.get_video_list_filepath(channel_id))
+
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
                                                                                                             channel_id)
-
         video_links = []
         url = first_url
         while True:
@@ -30,4 +34,17 @@ class GetVideoList(Step):
             except KeyError:
                 break
         print(video_links)
+        self.write_to_file(video_links, utils.get_video_list_filepath(channel_id))
+        return video_links
+
+    def write_to_file(self, video_links, filepath):
+        with open(filepath, 'w') as f:
+            for url in video_links:
+                f.write(url + '\n')
+
+    def read_file(self, filepath):
+        video_links = []
+        with open(filepath, 'r') as f:
+            for url in f:
+                video_links.append(url.strip())
         return video_links
